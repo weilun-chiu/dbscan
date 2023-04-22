3be266d33c39388023526eb969ea02f8a7a2abf2
navyblue1993 <john245678@gmail.com>
2023-04-22 19:02:02 -0400

    impl naive dbscan and grid dbscan. refactor.

--- dbscan.cpp
+++ dbscan.cpp
@@ -11,64 +11,19 @@
 #include <numeric>
 #include <set>
 #include <functional>
+#include <chrono>
+#include <queue>
 
 #include "utils.h"
 #include "point.h"
 #include "kdtree.h"
 #include "dbscan.h"
 
-std::tuple<std::vector<double>, std::vector<double>> calculateMinMaxValues(const std::vector<Point>& points) {
-    std::vector<double> max_values(Point::dimensionality, std::numeric_limits<double>::min());
-    std::vector<double> min_values(Point::dimensionality, std::numeric_limits<double>::max());
-    
-    for (const auto& point : points) {
-        for (int i = 0; i < Point::dimensionality; i++) {
-            max_values[i] = std::max(max_values[i], point[i]);
-            min_values[i] = std::min(min_values[i], point[i]);
-        }
-    }
 
-    return std::tie(max_values, min_values);
-}
 
-std::vector<double> normalize_point(const Point& point, const std::vector<double>& max_values, const std::vector<double>& min_values) {
-    std::vector<double> normalized_point(Point::dimensionality);
-    for (int i = 0; i < Point::dimensionality; i++) {
-        normalized_point[i] = (point[i] - min_values[i]) / (max_values[i] - min_values[i]);
-    }
-    return normalized_point;
-}
-
-std::vector<Point> normalize(const std::vector<Point>& points) {
-    std::vector<Point> result = points;
-
-    if (points.empty()) {
-        return result;
-    }
-
-    // Find the maximum and minimum values for each dimension
-    auto [max_values, min_values] = calculateMinMaxValues(points);
-
-    // Normalize the data points
-    for (int i = 0; i < points.size(); i++) {
-        std::vector<double> normalized_point = normalize_point(points[i], max_values, min_values);
-        result[i] = normalized_point;
-    }
-
-    return result;
-}
-
-std::vector<int> rangeQuery(std::vector<Point>& points, int p, double eps) {
-    std::vector<int> neighbors;
-    for (int i = 0; i < points.size(); i++) {
-        if (dist(points[p], points[i]) <= eps) {
-            neighbors.push_back(i);
-        }
-    }
-    return neighbors;
-}
-
-std::vector<int> naive_dbscan(std::vector<Point>& points, double eps, int minPts) {
+std::vector<int> NaiveDBSCAN::dbscan_algorithm(std::vector<Point> points) {
+    std::cout << "Naive DBSCAN(eps="<<eps<<", minPts="<<minPts<<") on datasize: " << points.size() << " in " << Point::dimensionality<<"-dimension space" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
     std::vector<int> visited(points.size(), 0);
     std::vector<int> cluster(points.size(), -1);
     int clusterIdx = 0;
@@ -98,7 +53,195 @@ std::vector<int> naive_dbscan(std::vector<Point>& points, double eps, int minPts
             clusterIdx++;
         }
     }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds." << std::endl;
     return cluster;
+}
+
+std::vector<int> getGridSize(const std::vector<double>& max_values, const std::vector<double>& min_values, double gridCellSize) {
+    std::vector<int> gridSize(max_values.size());
+    for (int i = 0; i < max_values.size(); i++) {
+        gridSize[i] = static_cast<int>(std::ceil((max_values[i] - min_values[i]) / gridCellSize));
+    }
+    return gridSize;
+}
+
+int kDTo1DIdx(const std::vector<int>& index, const std::vector<int>& gridSize) {
+    int k = index.size();
+    int Idx1D = index[0];
+    int stride = 1;
+    for (int i = 1; i < k; i++) {
+        stride *= gridSize[i-1];
+        Idx1D += stride * index[i];
+    }
+    return Idx1D;
+}
+
+std::vector<int> oneDToKDIdx(int index, const std::vector<int>& gridSize) {
+    std::vector<int> kDIdx(gridSize.size(), 0);
+    int k = gridSize.size();
+    for (int i = k - 1; i >= 0; i--) {
+        kDIdx[i] = index % gridSize[i];
+        index /= gridSize[i];
+    }
+    return kDIdx;
+}
+
+std::vector<int> getNeighborIndices(const std::vector<int>& index, const std::vector<int>& gridSize) {
+    std::vector<int> res{};
+    if (Point::dimensionality == 1) {
+        res = {index[0]-1, index[0]+1};
+    } else if (Point::dimensionality == 2) {
+        for (int i = std::max(0,index[0]-2); i < std::min(index[0]+3, gridSize[0]); i++) {
+            for (int j = std::max(0,index[1]-2); j < std::min(index[1]+3, gridSize[0]); j++) {
+                if ((abs(i)+abs(j)) == 4) continue;
+                if ((i == 0) && (j == 0)) continue;
+                res.push_back((i)*gridSize[0]+(j));
+            }
+        }
+    }
+    return res;
+}
+
+bool isConnect(std::vector<Point> lhs, std::vector<Point> rhs, double eps) {
+    for (const auto& lhsp: lhs)
+        for(const auto& rhsp: rhs)
+            if (dist(lhsp, rhsp) <= eps)
+                return true;
+    return false;
+}
+
+int getConnectCount(Point lhsp, std::vector<Point> rhs, double eps) {
+    int numConn{0};
+    for(const auto& rhsp: rhs)
+        if (dist(lhsp, rhsp) <= eps)
+            numConn++;
+    return numConn;
+}
+
+GridDBSCAN::GridDBSCAN(double _eps, int _minPts)
+    :   DBSCAN(_eps, _minPts) 
+    {}
+
+std::vector<Point> GridDBSCAN::preprocess(std::vector<Point> points) {
+    gridCellSize = eps / sqrt(Point::dimensionality);
+    auto [max_values, min_values] = calculateMinMaxValues(points);
+    gridSize = getGridSize(max_values, min_values, gridCellSize);
+    gridSize1D = std::accumulate(gridSize.begin(), gridSize.end(), 1, std::multiplies<int>());
+    grid.resize(gridSize1D);
+    corecell.resize(gridSize1D, false);
+    visited.resize(gridSize1D, 0);
+    cluster.resize(gridSize1D, -1);
+    clusterIdx = 0;
+    npoints = points.size();
+    return points;
+}
+void GridDBSCAN::assignPoints(std::vector<Point> points) {
+    for (const auto& p : points) {
+        std::vector<int> index(Point::dimensionality);
+        double _gridCellSize = gridCellSize;
+        std::transform(p.begin(), p.end(), index.begin(), [_gridCellSize](double x) {
+            return static_cast<int>(x / _gridCellSize);
+        });
+        int GridIndex1D(kDTo1DIdx(index, gridSize));
+        grid[GridIndex1D].push_back(p);
+    }
+}
+void GridDBSCAN::mark_ingrid_corecell() {
+    for (int i=0; i<gridSize1D; i++) {
+        if (grid[i].size() >= minPts) {
+            corecell[i] = true;
+        } 
+    }
+}
+void GridDBSCAN::mark_outgrid_corecell() {
+    for (int i=0; i<gridSize1D; i++) {
+        if (corecell[i] == true) continue;
+        if (grid[i].size() == 0) continue;
+        if (grid[i].size() > 0) 
+            corecell[i] = mark_outgrid_corecell_helper(i);
+    }
+}
+void GridDBSCAN::expand() {
+    for (int i = 0; i < gridSize1D; i++) {
+        if (visited[i] == true) continue;
+        visited[i] = true;
+        if (corecell[i] == false) continue;
+        cluster[i] = clusterIdx;
+        expand_helper(i);
+        clusterIdx++;
+    }
+}
+std::vector<int> GridDBSCAN::getClusterResults () {
+    std::vector<int> pointsCluster(npoints, -1);
+    for (int i=0; i<gridSize1D; i++) {
+        for (const auto& g: grid[i]) {
+            pointsCluster[g.id] = cluster[i];
+        }
+    }
+    return pointsCluster;
+}
+std::vector<int> GridDBSCAN::findNeighbor(int i) {
+    std::vector<int> kDIndex(oneDToKDIdx(i, gridSize));
+    std::vector<int> neighborsIn1D(getNeighborIndices(kDIndex, gridSize));
+    return neighborsIn1D;
+}
+bool GridDBSCAN::mark_outgrid_corecell_helper(int i) {
+    std::vector<int> neighbors=findNeighbor(i);
+    for (const auto& g: grid[i]) {
+        int numConn{0};
+        for (const auto& nid: neighbors) {
+            numConn += getConnectCount(g, grid[nid], eps);
+            if (numConn >= minPts) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+void GridDBSCAN::expand_helper(int i) {
+    std::queue<int> q;
+    q.push(i);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        std::vector<int> neighbors=findNeighbor(node);
+        for (auto& ni:neighbors) {
+            if (visited[ni] == true) continue;
+            if (isConnect(grid[i], grid[ni], eps)) {
+                visited[ni] = true;
+                cluster[ni] = clusterIdx;
+                if (corecell[ni]==true)
+                    q.push(ni);
+            }
+        }
+    }
+}
+
+std::vector<int> GridDBSCAN::dbscan_algorithm(std::vector<Point> points) {
+    if (Point::dimensionality > 2) {
+        std::cout << "Skip. Only support 2 dimension grid DBSCAN in this project." << std::endl;
+        return std::vector<int>(points.size(), -1);
+    }
+
+    std::cout << "Grid DBSCAN(eps="<<eps<<", minPts="<<minPts<<") on datasize: " << points.size() << " in " << Point::dimensionality<<"-dimension space" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Assign points to grid
+    assignPoints(points);
+    // Mark core cell
+    mark_ingrid_corecell();
+    mark_outgrid_corecell();
+    // Expand clustering
+    expand();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds." << std::endl;
+    
+    std::vector<int> pointsCluster = getClusterResults();
+    return pointsCluster;
 }
 
 std::vector<int> kdtree_dbscan(std::vector<Point>& points, double eps, int minPts) {
@@ -165,53 +308,16 @@ void print_clusters(const std::vector<Point>& points, const std::vector<int>& cl
     }
 }
 
-std::vector<Point> parseDataset(std::string filename) 
-{
-    std::ifstream infile(filename);
-    if (!infile.is_open()) {
-        throw std::runtime_error("Error opening file");
-    }
-
-    std::vector<Point> vp{};
-    std::string line{};
-    int idx{0};
-    while (std::getline(infile, line)) {
-        std::replace(line.begin(), line.end(), ',', ' ');
-
-        std::stringstream ss(line);
-        std::vector<double> values{std::istream_iterator<double>(ss), {}};
-        if (values.empty()) {
-            continue;
-        }
-
-        int label = static_cast<int>(values.back());
-        values.pop_back();
-        vp.emplace_back(Point(label, values, idx++));
-    }
-
-    return vp;
-}
-
-std::vector<Point> parseRandomGeneratedData(std::vector<std::vector<double>> generatedPoints) 
-{
-    std::vector<Point> vp{};
-    const int n = generatedPoints.size();
-    for (int i=0; i<n; i++) {
-        vp.emplace_back(Point(-1, generatedPoints[i], i));
-    }
-    return vp;
-}
-
-void DBSCANfromDataset::run() {
+void DBSCAN::run(std::string filename) {
     Point::resetDimension();
-    auto points(normalize(parseDataset(filename)));
-    auto cluster(naive_dbscan(points, eps, minPts));
-    print_clusters(points, cluster);
+    auto points(preprocess(normalize(parseDataset(filename))));
+    auto cluster(dbscan_algorithm(points));
+    // print_clusters(points, cluster);
 }
 
-void DBSCANfromGenerator::run(std::vector<std::vector<double>> generatedVectors) {
+void DBSCAN::run(std::vector<std::vector<double>> data) {
     Point::resetDimension();
-    auto points(normalize(parseRandomGeneratedData(generatedVectors)));
-    auto points_naive_clusters(naive_dbscan(points, eps, minPts));
-    print_clusters(points, points_naive_clusters);
+    auto points(preprocess(normalize(parseRandomGeneratedData(data))));
+    auto cluster(dbscan_algorithm(points));
+    // print_clusters(points, cluster);
 }
--- dbscan.h
+++ dbscan.h
@@ -8,28 +8,45 @@
 class DBSCAN {
 public:
     DBSCAN(double _eps, int _minPts) : eps(_eps), minPts(_minPts) {}
-    virtual ~DBSCAN() {}
-    virtual void run() = 0;
+    void run(std::string filename);
+    void run(std::vector<std::vector<double>>);
 protected:
     double eps;
     int minPts;
+    virtual std::vector<Point> preprocess(std::vector<Point> vp) = 0;
+    virtual std::vector<int> dbscan_algorithm(std::vector<Point> points) = 0;
 };
 
-class DBSCANfromDataset : public DBSCAN {
+class NaiveDBSCAN : public DBSCAN{
 public:
-    DBSCANfromDataset(const std::string& _filename, const double _eps, const int _minPts)
-        : DBSCAN(_eps, _minPts), filename(_filename) {}
-    void run() override;
-private:
-    std::string filename;
+    NaiveDBSCAN(double _eps, int _minPts) : DBSCAN(_eps, _minPts) {}
+protected:
+    std::vector<Point> preprocess(std::vector<Point> vp) override {return vp;}
+    std::vector<int> dbscan_algorithm(std::vector<Point> points) override;
 };
 
-class DBSCANfromGenerator : public DBSCAN {
+class GridDBSCAN : public DBSCAN{
 public:
-    DBSCANfromGenerator(double _eps, int _minPts)
-        : DBSCAN(_eps, _minPts) {}
-    void run(std::vector<std::vector<double>>);
+    GridDBSCAN(double _eps, int _minPts);
 private:
-    void run() override {};
+    int gridSize1D;
+    double gridCellSize;
+    std::vector<int> gridSize;
+    std::vector<std::vector<Point>> grid;
+    std::vector<bool> corecell;
+    std::vector<int> visited;
+    std::vector<int> cluster;
+    int clusterIdx;
+    int npoints;
+    void assignPoints(std::vector<Point> points);
+    void mark_ingrid_corecell();
+    void mark_outgrid_corecell();
+    void expand();
+    std::vector<int> getClusterResults();
+    std::vector<int> findNeighbor(int i);
+    bool mark_outgrid_corecell_helper(int i);
+    void expand_helper(int i);
+protected:
+    std::vector<Point> preprocess(std::vector<Point> vp) override;
+    std::vector<int> dbscan_algorithm(std::vector<Point> points) override;
 };
-
