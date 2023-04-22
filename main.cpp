dbaaf80360a415059b5829c9c9f3420b5590d75f
navyblue1993 <john245678@gmail.com>
2023-04-22 19:02:20 -0400

    update demo script.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,23 @@
 
 
 int main(int argc, char* argv[]) {
+    // Parameters
     double eps{0.12};
     int minPts{5};
     std::tie(eps, minPts) = parseDBSCANArguments(argc, argv);
-
-    DBSCANfromDataset A("iris_dataset.csv", eps, minPts);
-    A.run();
-
+    // Data generations
     DataGenerator generator(2, 1000);
-    DBSCANfromGenerator B(eps, minPts);
     auto cluster_data = generator.cluster_distribution(3);
-    B.run(cluster_data);
     auto uneven_data = generator.uneven_distribution(3, 1);
-    B.run(uneven_data);
-
+    // Naive DBSCAN
+    NaiveDBSCAN naive_dbscan(eps, minPts);
+    naive_dbscan.run("iris_dataset.csv");
+    naive_dbscan.run(cluster_data);
+    naive_dbscan.run(uneven_data);
+    // Grid DBSCAN
+    GridDBSCAN grid_dbscan(eps, minPts);
+    grid_dbscan.run("iris_dataset.csv");
+    grid_dbscan.run(cluster_data);
+    grid_dbscan.run(uneven_data);
     return 0;
 }
