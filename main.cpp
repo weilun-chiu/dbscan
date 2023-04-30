4076a7cb70b51d606c26b62ef3772e09a187df7f
navyblue1993 <john245678@gmail.com>
2023-04-30 10:17:00 -0400

    update script

--- main.cpp
+++ main.cpp
@@ -7,23 +7,30 @@ int main(int argc, char* argv[]) {
     // Parameters
     double eps{0.12};
     int minPts{5};
+    int n{1000000};
     std::tie(eps, minPts) = parseDBSCANArguments(argc, argv);
+
+    // NaiveDBSCAN naive_dbscan(eps, minPts);
+    // naive_dbscan.run("iris_dataset.csv");
+
     // Data generations
-    DataGenerator generator(2, 100000);
+    DataGenerator generator(2, n);
     auto cluster_data = generator.cluster_distribution(3);
-    // auto uneven_data = generator.uneven_distribution(3, 1);
-    // Grid DBSCAN
-    SerialGridDBSCAN serial_grid_dbscan(eps, minPts);
-    serial_grid_dbscan.run(cluster_data);
+    
+    // SerialGridDBSCAN serial_grid_dbscan(eps, minPts);
+    // serial_grid_dbscan.run(cluster_data);
 
     OMPGridDBSCAN omp_grid_dbscan(eps, minPts);
     omp_grid_dbscan.run(cluster_data);
 
-    ConcurrencyGridDBSCAN concurrency_grid_dbscan(eps, minPts);
-    concurrency_grid_dbscan.run(cluster_data);
+    // ConcurrencyGridDBSCAN concurrency_grid_dbscan(eps, minPts);
+    // concurrency_grid_dbscan.run(cluster_data);
 
     ConcurrencyStealingGridDBSCAN concurrency_stealing_grid_dbscan(eps, minPts);
     concurrency_stealing_grid_dbscan.run(cluster_data);
-    
+
+    ConcurrencyStealingAVX2GridDBSCAN concurrency_stealing_AVX_grid_dbscan(eps, minPts);
+    concurrency_stealing_AVX_grid_dbscan.run(cluster_data);
+
     return 0;
 }
