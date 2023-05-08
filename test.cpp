079cb6dfdaa6c80aeef4f21afa933d9223bd78c9
navyblue1993 <john245678@gmail.com>
2023-05-07 22:59:53 -0400

    update test script

--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include "src/point.h"
 #include <iostream>
+#include <cassert>
+#include <cmath>
 
 int main() {
     // Create a Point object
@@ -7,11 +9,13 @@ int main() {
     Point p(1, coords, 0);
 
     // Test the size() function
+    assert(p.size() == 3);
     std::cout << "Size of point: " << p.size() << '\n';
 
     // Test the dist() function
     std::vector<double> coords2 = {2.0, 4.0, 6.0};
     double d = p.dist(coords2);
+    assert(std::abs(d - 3.74166) < 1e-5);
     std::cout << "Distance between points: " << d << '\n';
 
     // Test the += operator
@@ -29,10 +33,15 @@ int main() {
     p /= divisor;
     std::cout << "New coordinates after division: " << p << '\n';
 
-    // Test the /= operator
+    // Test the /= operator with zero divisor
     double divisorzero = 0.0;
-    p /= divisorzero;
+    try {
+        p /= divisorzero;
+    } catch (const std::runtime_error& e) {
+        std::cout << "Caught exception: " << e.what() << '\n';
+    }
     std::cout << "New coordinates after division 0: " << p << '\n';
 
+    std::cout << "All tests passed." << std::endl;
     return 0;
 }
